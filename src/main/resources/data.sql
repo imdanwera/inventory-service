fe03665311d3bc0f62d228a4e943647a42c60826
imdanwera <im.danwera@gmail.com>
2020-09-28 18:52:32 +0530

    Added Accessories to db script.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,4 +11,8 @@ CREATE TABLE IF NOT EXISTS `Inventory` (
 INSERT INTO `Inventory` (`id`, `Product_Name`, `Product_Description`, `Product_Type`, `Product_Accessory_Type`) VALUES
 ('1', 'SAMSUNG GALAXY A80', 'SAMSUNG GALAXY A80', 'MOBILE', null ),
 ('2', 'SAMSUNG GALAXY A20', 'SAMSUNG GALAXY A20', 'MOBILE', null ),
-('3', 'SAMSUNG GALAXY A10', 'SAMSUNG GALAXY A10', 'MOBILE', null );
+('3', 'SAMSUNG GALAXY A10', 'SAMSUNG GALAXY A10', 'MOBILE', null ),
+('4', 'Ferrari Back Cover', 'Back Cover', 'COVER', 'MOBILE' ),
+('5', 'WKL USB Charger', 'USB Charger', 'CHARGER', 'MOBILE' ),
+('6', 'JBL Charge 4', 'JBL Charge 4', 'SPEAKER', 'MOBILE' ),
+('7', 'Beats Solo3', 'Beats Solo3', 'HEAD_SET', 'MOBILE' );
